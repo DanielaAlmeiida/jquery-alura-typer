5bc21b0cae9d1d0a05e5d5bfe42c00843149d458
Daniela Almeida <danielaspbrasil@gmail.com>
2023-07-28 23:25:23 -0300

    Limpando e arrumando o código

--- public/js/main.js
+++ public/js/main.js
@@ -3,21 +3,13 @@ var text =  $('.text').text();
 var textLength = text.split(" ").length;
 
 $('.text-length').text(textLength);
-
-
+$('.character-length').text(text.length);
 
 var field = $('.text-field')
 
 field.on("click", function(){
     var text = field.val().trim();
-    console.log(text)
-
     var textLength = text.split(/\S+/).length - 1;
-    console.log(textLength)
-    console.log("Quantidade de palavras: " + textLength);
-    console.log("Quantidade de caracteres: " + text.length);
-
-
 
     $('.word-counter').text(textLength);
     $('.character-counter').text(text.length)
