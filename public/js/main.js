a84b5cbb68e8dd8f5a69193760421d35cd667a99
Daniela Almeida <danielaspbrasil@gmail.com>
2023-07-30 00:22:03 -0300

    Novas funcionalidades e layout

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,79 @@
-var text =  $('.text').text();
+var textField = $('#text-field');
+var initialTime = $('#typing-time').text();
+var restartButton = $('#restart-button');
 
-var textLength = text.split(" ").length;
-
-$('.text-length').text(textLength);
-$('.character-length').text(text.length);
-
-var field = $('.text-field')
-
-field.on("click", function(){
-    var text = field.val().trim();
-    var textLength = text.split(/\S+/).length - 1;
-
-    $('.word-counter').text(textLength);
-    $('.character-counter').text(text.length)
-
+$(function() {
+    updatePhraseLength();
+    startCounters();
+    startChronometer();
+    borderControl();
+    restartButton.click(restartGame);
 });
 
 
+function updatePhraseLength() {
+    var phrase =  $('#phrase').text();
+    var phraseLength = phrase.split(" ").length;
+    $('#phrase-length').text(phraseLength);
+    $('#character-phrase-length').text(phrase.length);
+}
 
 
+function startCounters() {
+    textField.on("input", function(){
+        var text = textField.val().trim();
+        var textLength = text.split(/\S+/).length - 1;
+    
+        $('#word-counter').text(textLength);
+        $('#character-counter').text(text.length);
+    });
+}
 
 
+function startChronometer() {
+    var remainingTypingTime = $('#typing-time').text();
+    restartButton.attr("disabled", true);
+
+    textField.one("focus", function(){   
+        var idChronotemer = setInterval(function(){
+            remainingTypingTime--;
+            $("#typing-time").text(remainingTypingTime);
+            
+            if(remainingTypingTime < 1){
+                textField.attr("disabled", true);
+                clearInterval(idChronotemer);
+                restartButton.attr("disabled", false);
+            }
+    
+        }, 1000);
+    })
+}
 
 
+function restartGame() {
+    textField.attr("disabled", false);
+    textField.val("");
+    $('#word-counter').text(0);
+    $('#character-counter').text(0);
+    $('#typing-time').text(initialTime);  
+    startChronometer(); 
+    textField.removeClass("green-border");
+    textField.removeClass("red-border");
+}
 
+function borderControl(){
+    var phrase = $('#phrase').text();
 
+    textField.on("input", function(){
+        var typing = textField.val();
 
+        if(phrase.startsWith(typing)) {
+            textField.removeClass("red-border")
+            textField.addClass("green-border")
+        } else {
+            textField.removeClass("green-border")
+            textField.addClass("red-border")
+        }
+    })
+}
 
-
-
-
-
-
-
-
-
-console.log(
-    field
-)
